1c0331b34df3e7bf3f2533cbbde58a965719377a
Bondue11e <91042295+Bondue11e@users.noreply.github.com>
2021-09-20 06:40:52 +0300

    Add files via upload

---
+++ task_queue_demo.cpp
@@ -0,0 +1,52 @@
+﻿#include <functional>
+#include <thread>
+#include <condition_variable>
+#include <iostream>
+
+class task_queue {
+
+public:
+
+	explicit task_queue(int worker_count) {
+		// TODO: multiple workers
+		worker = std::thread([this]() {
+			std::unique_lock<std::mutex> l(m);
+			for (;;) {
+				if (done)
+					break;
+				cv.wait(l);
+			}
+		});
+	}
+
+	~task_queue() {
+		{
+			std::lock_guard<std::mutex> l(m);
+			done = true;
+			cv.notify_all();
+		}
+		worker.join();
+	}
+
+	void enqueue(std::function<void()> task) {
+		// TODO
+	}
+
+private:
+
+	std::thread worker;
+
+	std::mutex m;
+	std::condition_variable cv;
+	bool done = false;
+
+};
+
+// TODO proper tests
+
+int main() {
+	task_queue q(10);
+	for (int i = 0; i != 20; ++i)
+		q.enqueue([i]() { std::cout << "hello world " << i << "\n"; });
+	return 0;
+}
